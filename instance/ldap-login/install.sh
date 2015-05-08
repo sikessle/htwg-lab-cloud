688b320b15c9aceec7cf7f51d1935986cd6ba241
Simon Kessler <sikessle@htwg-konstanz.de>
2015-05-08 21:13:31 +0200

    refactored code. more readable now

--- instance/ldap-login/install.sh
+++ instance/ldap-login/install.sh
@@ -8,29 +8,38 @@ if [ "$(whoami)" != "root" ]; then
 fi
 
 USER=$1
+BASEDN="ou=users,dc=fh-konstanz,dc=de"
+LDAP_CONF=/etc/ldap.conf
 
-echo "configuring ldap for user $USER"
+echo "configuring ldap for user: $USER"
 
 echo "installing ldap packages"
 DEBIAN_FRONTEND=noninteractive apt-get -y -q install ldap-auth-client nscd
 
 echo "configuring ldap config files"
-echo "base ou=users,dc=fh-konstanz,dc=de\nuri ldap://ldap.htwg-konstanz.de/\nldap_version 3\npam_password md5\nnss_override_attribute_value homeDirectory /home/$USER\nnss_override_attribute_value gidNumber 2000\nnss_override_attribute_value uidNumber 2000\nnss_override_attribute_value loginShell /bin/bash\n" > /etc/ldap.conf
+echo "" > $LDAP_CONF
+echo "base $BASEDN" >> $LDAP_CONF
+echo "uri ldap://ldap.htwg-konstanz.de/" >> $LDAP_CONF
+echo "ldap_version 3" >> $LDAP_CONF
+echo "pam_password md5" >> $LDAP_CONF
+echo "nss_override_attribute_value homeDirectory /home/$USER" >> $LDAP_CONF
+echo "nss_override_attribute_value gidNumber 2000" >> $LDAP_CONF
+echo "nss_override_attribute_value uidNumber 2000" >> $LDAP_CONF
+echo "nss_override_attribute_value loginShell /bin/bash" >> $LDAP_CONF
+
+echo "restricting access from LDAP to single user: $USER"
+echo "nss_base_passwd uid=$USER,$BASEDN" >> $LDAP_CONF
 
 echo "setup auth services to look in ldap"
 auth-client-config -t nss -p lac_ldap
 
 echo "configure to create home folder on login"
-echo "\nsession required\tpam_mkhomedir.so skel=/etc/skel umask=0022\n" > /etc/pam.d/common-session
+echo "\nsession required\tpam_mkhomedir.so skel=/etc/skel umask=0022" > /etc/pam.d/common-session
 
 echo "assigning ldap users to local groups"
-echo "\n*;*;*;Al0000-2400;adm,cdrom,sudo,dip,plugdev,lpadmin,sambashare\n" >> /etc/security/group.conf 
-echo "\n\nauth\trequired\tpam_group.so use_first_pass\n" >> /etc/pam.d/common-auth
-echo "\n$USER:x:2000:\n" >> /etc/group
-
-
-echo "restricting access from LDAP to single user $USER"
-echo "\nnss_base_passwd uid=$USER,ou=users,dc=fh-konstanz,dc=de\n" >> /etc/ldap.conf
+echo "\n*;*;*;Al0000-2400;adm,cdrom,sudo,dip,plugdev,lpadmin,sambashare" >> /etc/security/group.conf 
+echo "\n\nauth\trequired\tpam_group.so use_first_pass" >> /etc/pam.d/common-auth
+echo "\n$USER:x:2000:" >> /etc/group
 
 echo "restarting naming service"
 /etc/init.d/nscd restart
