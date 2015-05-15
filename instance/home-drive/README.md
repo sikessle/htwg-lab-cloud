3369d42718836bb0f0d34a78ec143753e6f3a764
Simon Kessler <sikessle@htwg-konstanz.de>
2015-05-15 19:46:44 +0200

    auto mounting home-drive on login

--- instance/home-drive/README.md
+++ instance/home-drive/README.md
@@ -1,10 +1,23 @@
 # Mount home drive from HTWG
 
-The user's home drive from the HTWG must be mounted on boot up.
+The user's home drive from the HTWG must be mounted on login.
 
-- Mount home drive (z-drive) at startup time as "home-drive"
+- Mount home drive (z-drive) at login as "home-drive"
 - Add directory to sidebar in nautilus
 
 ## HTWG home drive
 
 Lies in `smb://homedrive.htwg-konstanz.de/home`
+
+## Required Packages
+
+- libpam_mount
+- cifs-utils
+
+## Script notes
+
+Involved files: 
+
+- /etc/security/pam_mount.conf.xml
+- ~/pam_mount.conf.xml (created)
+
--- instance/home-drive/install.sh
+++ instance/home-drive/install.sh
@@ -8,5 +8,33 @@ if [ "$(whoami)" != "root" ]; then
 fi
 
 USER=$1
+MNT_POINT=/mnt/home-drive
+PAM_LOCAL_CONF=/home/$USER/.pam_mount.conf.xml
+PAM_GLOBAL_CONF=/etc/security/pam_mount.conf.xml
 
+echo "installing required packages"
+apt-get install -y libpam_mount cifs-utils
 
+echo "configuing auto-mounting of home-drive"
+mkdir $MNT_POINT
+touch $PAM_LOCAL_CONF
+
+echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>" >> $PAM_LOCAL_CONF
+echo "<!DOCTYPE pam_mount SYSTEM \"pam_mount.conf.xml.dtd\">" >> $PAM_LOCAL_CONF
+echo "<pam_mount>" >> $PAM_LOCAL_CONF
+echo "<volume options=\"user=$USER,workgroup=FHKN,domain=FHKN\" user=\"$USER\" mountpoint=\"/media/home-drive\" path=\"//homedrive.htwg-konstanz.de/home\" fstype=\"cifs\" />" >> $PAM_LOCAL_CONF
+echo "</pam_mount>" >> $PAM_LOCAL_CONF
+
+echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>" >> $PAM_GLOBAL_CONF
+echo "<!DOCTYPE pam_mount SYSTEM \"pam_mount.conf.xml.dtd\">" >> $PAM_GLOBAL_CONF
+echo "<pam_mount>" >> $PAM_GLOBAL_CONF
+echo "<debug enable=\"0\" />" >> $PAM_GLOBAL_CONF
+echo "<luserconf name=\".pam_mount.conf.xml\" />" >> $PAM_GLOBAL_CONF
+echo "<mntoptions require=\"\" />" >> $PAM_GLOBAL_CONF
+echo "<mntoptions allow=\"*\" />" >> $PAM_GLOBAL_CONF
+echo "<!-- mntoptions deny=\"suid,dev\" / -->" >> $PAM_GLOBAL_CONF
+echo "<!-- mntoptions require=\"nosuid,nodev\" / -->" >> $PAM_GLOBAL_CONF
+echo "<path>/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin</path>" >> $PAM_GLOBAL_CONF
+echo "<logout wait=\"0\" hup=\"0\" term=\"0\" kill=\"0\" />" >> $PAM_GLOBAL_CONF
+echo "<mkmountpoint enable=\"1\" remove=\"true\" />" >> $PAM_GLOBAL_CONF
+echo "</pam_mount>" >> $PAM_GLOBAL_CONF
