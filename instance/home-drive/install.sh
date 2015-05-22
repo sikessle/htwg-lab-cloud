cdbe125f33901250449acbbe7d1419cf05eddc80
Simon Kessler <sikessle@htwg-konstanz.de>
2015-05-22 20:22:02 +0200

    bugfix for home-drive: tried to access not-yet created home folder

--- instance/home-drive/install.sh
+++ instance/home-drive/install.sh
@@ -1,7 +1,6 @@
 # This script is only a part of a the main script in the superfolder!
 
 MNT_POINT=/mnt/home-drive
-PAM_LOCAL_CONF=/home/$USER/.pam_mount.conf.xml
 PAM_GLOBAL_CONF=/etc/security/pam_mount.conf.xml
 
 echo "installing required packages"
@@ -9,19 +8,12 @@ apt-get install -y libpam-mount cifs-utils
 
 echo "configuring auto-mounting of home-drive"
 mkdir $MNT_POINT
-touch $PAM_LOCAL_CONF
-
-echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>" >> $PAM_LOCAL_CONF
-echo "<!DOCTYPE pam_mount SYSTEM \"pam_mount.conf.xml.dtd\">" >> $PAM_LOCAL_CONF
-echo "<pam_mount>" >> $PAM_LOCAL_CONF
-echo "<volume options=\"user=$USER,workgroup=FHKN,domain=FHKN\" user=\"$USER\" mountpoint=\"/media/home-drive\" path=\"//homedrive.htwg-konstanz.de/home\" fstype=\"cifs\" />" >> $PAM_LOCAL_CONF
-echo "</pam_mount>" >> $PAM_LOCAL_CONF
 
 echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>" >> $PAM_GLOBAL_CONF
 echo "<!DOCTYPE pam_mount SYSTEM \"pam_mount.conf.xml.dtd\">" >> $PAM_GLOBAL_CONF
 echo "<pam_mount>" >> $PAM_GLOBAL_CONF
 echo "<debug enable=\"0\" />" >> $PAM_GLOBAL_CONF
-echo "<luserconf name=\".pam_mount.conf.xml\" />" >> $PAM_GLOBAL_CONF
+echo "<volume options=\"user=$USER,workgroup=FHKN,domain=FHKN\" user=\"$USER\" mountpoint=\"/media/home-drive\" path=\"//homedrive.htwg-konstanz.de/home\" fstype=\"cifs\" />" >> $PAM_GLOBAL_CONF
 echo "<mntoptions require=\"\" />" >> $PAM_GLOBAL_CONF
 echo "<mntoptions allow=\"*\" />" >> $PAM_GLOBAL_CONF
 echo "<!-- mntoptions deny=\"suid,dev\" / -->" >> $PAM_GLOBAL_CONF
