37dc606ea9549623830186ff26da380f82e78b00
Simon Kessler <sikessle@htwg-konstanz.de>
2015-05-23 11:27:14 +0200

    disabling password requirement

--- instance/remote-desktop/install.sh
+++ instance/remote-desktop/install.sh
@@ -3,17 +3,17 @@
 X11_CONF="/etc/init/x11vnc.conf"
 
 echo "installing x11vnc"
-apt-get install x11vnc
+apt-get install -y x11vnc
 
-echo "configuring password"
-mkdir /etc/x11vnc
-x11vnc -storepasswd in /etc/x11vnc/passwd
-x11vnc -storepasswd /etc/x11vnc/passwd
-# enter the password you want to set for VNC access
-# choose y to save pass
+# Uncomment to enable passwords (beware: not yet an unattended install)
+#echo "configuring password"
+#mkdir /etc/x11vnc
+#x11vnc -storepasswd in /etc/x11vnc/passwd
+#x11vnc -storepasswd /etc/x11vnc/passwd
 
 echo "configuring lightdm and vnc"
 echo "start on login-session-start" > $X11_CONF
 echo "script" >> $X11_CONF
-echo "sudo x11vnc -xkb -forever -auth /var/run/lightdm/root/:0 -display :0 -rfbauth /etc/x11vnc/passwd -rfbport 5900 -bg -o /var/log/x11vnc.log" >> $X11_CONF
+# To use a password, add to the next line as paramter: -rfbauth /etc/x11vnc/passwd
+echo "sudo x11vnc -xkb -forever -auth /var/run/lightdm/root/:0 -display :0 -rfbport 5900 -bg -o /var/log/x11vnc.log" >> $X11_CONF
 echo "end script" >> $X11_CONF
