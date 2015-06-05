bf7424c4d492759d6f4241d336822fa1148b4bd1
Simon Kessler <sikessle@htwg-konstanz.de>
2015-06-05 10:10:54 +0200

    removed static assignment of image path for local testing

--- instance/image/image-virtualbox-to-openstack.sh
+++ instance/image/image-virtualbox-to-openstack.sh
@@ -8,7 +8,7 @@ if [ "$IMAGE" = "" ]; then
 	exit 1
 fi
 
-IMAGE="/Users/sikessle/Documents/Software/Virtual Machines/OpenStack/Cloud-Image Ubuntu/Snapshots/{9ce5f09a-333f-4136-b5b2-d45f6b519749}.vdi"
+#IMAGE="/Users/sikessle/Documents/Software/Virtual Machines/OpenStack/Cloud-Image Ubuntu/Snapshots/{9ce5f09a-333f-4136-b5b2-d45f6b519749}.vdi"
 
 echo "***************************************************"
 echo "converting $IMAGE to openstack compatible format"
