47dc5bcb6dab7142f5af69a9f44cec4696160f2a
Simon Kessler <sikessle@htwg-konstanz.de>
2015-06-12 13:28:38 +0200

    bugfix: tab not evaluated in variable. also chmod 777 on every mount

--- instance/block-storage/install.sh
+++ instance/block-storage/install.sh
@@ -2,18 +2,19 @@
 
 LAB_DRIVE_SCRIPT="/usr/local/bin/mount-lab-drive.sh"
 LAB_DRIVE_MNT_POINT="/media/lab-drive"
-LAB_DRIVE_MNT_FSTAB="/dev/vdb\t$LAB_DRIVE_MNT_POINT\text4\trw,suid,dev,exec,auto,users,async\t0\t0"
+LAB_DRIVE_MNT_FSTAB="/dev/vdb	$LAB_DRIVE_MNT_POINT	ext4	rw,suid,dev,exec,auto,users,async	0	0"
 
 echo "creating mount script"
 
 echo "#!/bin/bash" >> $LAB_DRIVE_SCRIPT
 echo "# check current fstype. WARNING: MUST be running under sudo, otherwise not showing up!" >> $LAB_DRIVE_SCRIPT
-echo "LAB_DRIVE_FSTYPE=$(blkid -o value -s TYPE /dev/vdb)" >> $LAB_DRIVE_SCRIPT
+echo "LAB_DRIVE_FSTYPE=\$(blkid -o value -s TYPE /dev/vdb)" >> $LAB_DRIVE_SCRIPT
 echo "if [ \"\$LAB_DRIVE_FSTYPE\" != \"ext4\" ]; then" >> $LAB_DRIVE_SCRIPT
 echo "	# create filesystem" >> $LAB_DRIVE_SCRIPT
 echo "	mkfs.ext4 /dev/vdb" >> $LAB_DRIVE_SCRIPT
 echo "fi" >> $LAB_DRIVE_SCRIPT
 echo "mount -a" >> $LAB_DRIVE_SCRIPT
+echo "chmod 777 $LAB_DRIVE_MNT_POINT" >> $LAB_DRIVE_SCRIPT
 
 chmod +x $LAB_DRIVE_SCRIPT
 
@@ -24,11 +25,12 @@ mkdir $LAB_DRIVE_MNT_POINT
 chmod 777 $LAB_DRIVE_MNT_POINT
 
 echo "adjusting /etc/fstab"
-echo "$LAB_DRIVE_MNT_FSTAB\n" >> /etc/fstab
+echo "$LAB_DRIVE_MNT_FSTAB" >> /etc/fstab
+echo "" >> /etc/fstab
 
 echo "configuring udev"
-echo "SUBSYSTEM==\"block\", RUN+=\"$LAB_DRIVE_SCRIPT\"\n" >> /etc/udev/rules.d/80-mount.rules
-echo "ENV{ID_FS_USAGE}==\"filesystem|other|crypto\", ENV{UDISKS_FILESYSTEM_SHARED}=\"1\"\n" >> /etc/udev/rules.d/99-udisks2.rules
+echo "SUBSYSTEM==\"block\", RUN+=\"$LAB_DRIVE_SCRIPT\"" >> /etc/udev/rules.d/80-mount.rules
+echo "ENV{ID_FS_USAGE}==\"filesystem|other|crypto\", ENV{UDISKS_FILESYSTEM_SHARED}=\"1\"" >> /etc/udev/rules.d/99-udisks2.rules
 
 echo "reload rules for udev"
 udevadm control --reload-rules
