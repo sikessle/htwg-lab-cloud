de6cd064da05ba74468ebff7a90d590ea186b7a8
Simon Kessler <sikessle@htwg-konstanz.de>
2015-06-20 14:13:18 +0200

    disabling of network manager and network interfaces setup file

--- devstack/nova-network/deploy.sh
+++ devstack/nova-network/deploy.sh
@@ -3,8 +3,8 @@
 # Deploys this feature
 
 # Disabling network manager
-#sudo stop network-manager
-#echo "manual" | sudo tee /etc/init/network-manager.override
+sudo stop network-manager
+echo "manual" | sudo tee /etc/init/network-manager.override
 
-#sudo cp interfaces.txt /etc/network/interfaces
-#sudo ifdown --exclude=lo -a && sudo ifup --exclude=lo -a
+sudo cp interfaces.txt /etc/network/interfaces
+sudo ifdown --exclude=lo -a && sudo ifup --exclude=lo -a
