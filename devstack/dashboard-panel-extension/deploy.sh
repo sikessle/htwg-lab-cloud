3183b5b194af1db74f6a7e9185d5f76bdccc017c
Simon Kessler <sikessle@htwg-konstanz.de>
2015-06-20 11:37:34 +0200

    deployment of network and dashboard extension

--- devstack/dashboard-panel-extension/deploy.sh
+++ devstack/dashboard-panel-extension/deploy.sh
@@ -2,3 +2,6 @@
 
 # Deploys this feature
 
+cp prof /opt/stack/horizon/openstack_dashboard/dashboards/
+cp "_50_prof.py" "/opt/stack/horizon/openstack_dashboard/enabled/"
+sudo service apache2 restart
--- devstack/nova-network/deploy.sh
+++ devstack/nova-network/deploy.sh
@@ -2,3 +2,9 @@
 
 # Deploys this feature
 
+# Disabling network manager
+sudo stop network-manager
+echo "manual" | sudo tee /etc/init/network-manager.override
+
+sudo cp interfaces /etc/network/interfaces
+sudo ifdown --exclude=lo -a && sudo ifup --exclude=lo -a
