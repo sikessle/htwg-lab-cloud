7d6da1409d4ff3447f6c202c98ad3bf8f34313fa
Simon Kessler <sikessle@htwg-konstanz.de>
2015-06-20 11:24:52 +0200

    fix in cp

--- devstack/dashboard-theme/deploy.sh
+++ devstack/dashboard-theme/deploy.sh
@@ -3,7 +3,7 @@
 # Deploys this feature
 
 cp local_settings.py horizon/openstack_dashboard/local/
-cp lab-cloud /opt/stack/horizon/openstack_dashboard/static/themes/
+cp -r lab-cloud /opt/stack/horizon/openstack_dashboard/static/themes/
 python /opt/stack/horizon/manage.py collectstatic --noinput --clear
 python /opt/stack/horizon/manage.py compress --force
 sudo service apache2 restart
