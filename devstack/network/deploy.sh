3349db8c331f55dc80524ba94a0952e69047d5e8
Simon Kessler <sikessle@htwg-konstanz.de>
2015-06-20 20:13:20 +0200

    network settings

--- devstack/network/deploy.sh
+++ devstack/network/deploy.sh
@@ -3,8 +3,10 @@
 # Deploys feature
 
 # Security group rules
+cwd=$(pwd)
 cd ~/devstack
 source openrc admin admin
-nova secgroup-add-rule default tcp 22 22 0.0.0.0/0
+nova secgroup-add-rule default tcp -1 -1 0.0.0.0/0
+nova secgroup-add-rule default udp -1 -1 0.0.0.0/0
 nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
-cd ..
+cd cwd
