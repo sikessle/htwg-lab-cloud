f5c8c10f0d78adaeb7e12d6c95f6f263ea4fa446
Simon Kessler <sikessle@htwg-konstanz.de>
2015-06-20 21:09:04 +0200

    bugfix: forgot $, ports fixed

--- devstack/network/deploy.sh
+++ devstack/network/deploy.sh
@@ -6,7 +6,7 @@
 cwd=$(pwd)
 cd ~/devstack
 source openrc admin admin
-nova secgroup-add-rule default tcp -1 -1 0.0.0.0/0
-nova secgroup-add-rule default udp -1 -1 0.0.0.0/0
+nova secgroup-add-rule default tcp 1 65535 0.0.0.0/0
+nova secgroup-add-rule default udp 1 65535 0.0.0.0/0
 nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
-cd cwd
+cd $cwd
