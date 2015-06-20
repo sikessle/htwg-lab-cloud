476e22d1ea3101776d0820a4108e1db86b9a066e
Simon Kessler <sikessle@htwg-konstanz.de>
2015-06-20 11:56:06 +0200

    copying instance-setup.sh to /opt/stack/htwg

--- instance/deploy.sh
+++ instance/deploy.sh
@@ -5,8 +5,8 @@
 IMAGE_DL="https://www.dropbox.com/sh/0dgcaxc2u7cbfgi/AABhaR7-cTC_JcWDwWAtloO3a/ubuntu-14.04-openstack-qcow2.img?dl=1"
 
 make
-echo "TODO: Where must this script be copied to?"
-cp instance-setup.sh ~/
+mkdir /opt/stack/htwg
+cp instance-setup.sh /opt/stack/htwg/
 
 echo "Uncomment to enable image download"
 #wget -O ~/ubuntu-14.04-openstack-qcow2.img "$IMAGE_DL"
