e4b3fe4ad68bfbe0ac842d39ff514e87ea386716
Simon Kessler <sikessle@htwg-konstanz.de>
2015-06-20 11:14:09 +0200

    deployment of devstack

--- devstack/deploy.sh
+++ devstack/deploy.sh
@@ -2,3 +2,16 @@
 
 # Deploys devstack
 
+# setup network adapters
+nova-network/deploy.sh
+
+# install devstack
+git clone https://github.com/openstack-dev/devstack.git -b stable/kilo ~/devstack/
+cp "local.conf" ~/devstack/
+~/devstack/stack.sh
+sed -i "s/OFFLINE=False/OFFLINE=True/g" ~/devstack/local.conf 
+
+# install dashboard customizations
+dashboard-theme/deploy.sh
+dashboard-panel-extension/deploy.sh
+
