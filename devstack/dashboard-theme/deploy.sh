10dc4b00f24ced992ea3e8aa68898f0878982065
Simon Kessler <sikessle@htwg-konstanz.de>
2015-06-20 11:22:13 +0200

    deployment of dashboard theme

--- devstack/dashboard-theme/deploy.sh
+++ devstack/dashboard-theme/deploy.sh
@@ -2,3 +2,8 @@
 
 # Deploys this feature
 
+cp local_settings.py horizon/openstack_dashboard/local/
+cp lab-cloud /opt/stack/horizon/openstack_dashboard/static/themes/
+python /opt/stack/horizon/manage.py collectstatic --noinput --clear
+python /opt/stack/horizon/manage.py compress --force
+sudo service apache2 restart
