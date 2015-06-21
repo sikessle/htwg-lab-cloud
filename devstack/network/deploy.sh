1615f30489eb7c53a1447c24f63fd33df94f8721
Simon Kessler <sikessle@htwg-konstanz.de>
2015-06-21 12:23:07 +0200

    internet in instances working

--- devstack/network/deploy.sh
+++ devstack/network/deploy.sh
@@ -10,3 +10,7 @@ nova secgroup-add-rule default tcp 1 65535 0.0.0.0/0
 nova secgroup-add-rule default udp 1 65535 0.0.0.0/0
 nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
 cd $cwd
+
+# Enable internet in instances. 
+# SNAT: sending outgoing traffic over eth0 just before it is sended.
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
