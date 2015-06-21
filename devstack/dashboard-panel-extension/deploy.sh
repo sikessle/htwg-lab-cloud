250c51fda1e755d5d85c54c5d2e024592970915a
Simon Kessler <sikessle@htwg-konstanz.de>
2015-06-21 21:27:43 +0200

    removed apache2 stop. this will break the restarting as the pids change.

--- devstack/dashboard-panel-extension/deploy.sh
+++ devstack/dashboard-panel-extension/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Deploys this feature
-sudo service apache2 stop
+
 cp -r prof /opt/stack/horizon/openstack_dashboard/dashboards/
 cp "_50_prof.py" /opt/stack/horizon/openstack_dashboard/enabled/
 sudo service apache2 restart
